c21d9616adde3195b1bda22c8f4467918522c02a
Veyler <tigarr80@yahoo.com>
2026-05-10 23:12:17 +0200

    zoijdhfvjc,

--- client/js/render.js
+++ client/js/render.js
@@ -1,3 +1,7 @@
+if (!localStorage.getItem("authToken")) {
+    window.location.href = "/pages/login.html"
+}
+
 const token = localStorage.getItem("authToken")
 const socket = io("https://la-taverne-cjm6.onrender.com", {
     auth : { token : token}
@@ -14,14 +18,12 @@ socket.on("message", text => {
 
 function send_clicked(x){
     var text = document.getElementById("InputMessage").value
-
     console.log("Send Button clicked")
     console.log("Message : " + text)
-
     socket.emit("message", text)
 }
 
 function disconnect_clicked(x){
     localStorage.removeItem("authToken")
-    window.location.href="http://127.0.0.1:5500/client/pages/login.html"
+    window.location.href = "/pages/login.html"  
 }
